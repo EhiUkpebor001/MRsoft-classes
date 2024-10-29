cc23d0ffc43a4505e968e6b2380785f9eed70e0d
Ukpebor Ehiremhen <ehiremhenukpebor@gmail.com>
2024-10-29 15:16:15 +0100

    Delete setup.sh

--- setup.sh
+++
@@ -1,7 +0,0 @@
-echo "# MRsoft-classes" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/EhiUkpebor001/MRsoft-classes.git
-git push -u origin main
